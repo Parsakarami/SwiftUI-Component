b623ffcfb611bab5694dc540527c1839a766ab1d
Parsa Karami <Parsakarami@live.com>
2024-01-27 09:53:55 -0500

    Initial Commit

---
+++ SwiftUI-Component/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Component
+//
+//  Created by Parsa Karami on 2024-01-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Component/SwiftUI_ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ComponentApp.swift
+//  SwiftUI-Component
+//
+//  Created by Parsa Karami on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
