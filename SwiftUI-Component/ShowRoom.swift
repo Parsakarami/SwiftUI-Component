c529e2374b0871aa4873f079973a501695d96f3f
Parsa Karami <Parsakarami@live.com>
2024-01-29 15:00:32 -0500

    Add navigation view to the show room

--- SwiftUI-Component/ShowRoom.swift
+++ SwiftUI-Component/ShowRoom.swift
@@ -8,37 +8,23 @@
 import SwiftUI
 
 struct ShowRoom: View {
-    @State var showSideMenu : Bool = false
+    
     var body: some View {
-        ZStack {
-            VStack {
-                Button(action: {
-                    withAnimation(.snappy) {
-                        showSideMenu.toggle()
+        NavigationView {
+            ZStack {
+                VStack {
+                    NavigationLink {
+                        SideMenuView()
+                    } label: {
+                        Text("Side Menu")
+                            .frame(width: 200,height:40)
+                            .background(.indigo)
+                            .foregroundStyle(.white)
+                            .clipShape(.capsule)
                     }
-                }, label: {
-                    Text("Side Menu")
-                        .frame(width: 200,height:40)
-                        .background(.indigo)
-                        .foregroundStyle(.white)
-                        .clipShape(.capsule)
-                })
+                }
+                .frame(maxWidth: .infinity ,maxHeight: .infinity)
             }
-            .frame(maxWidth: .infinity ,maxHeight: .infinity)
-            .opacity(showSideMenu ? 0.75 : 1)
-            .blur(radius: showSideMenu ? 3 : 0)
-            .zIndex(0)
-            
-            SideMenu(
-                isShowMenu: $showSideMenu,
-                userName: "Jenna Smith",
-                profile: "Profile",
-                bgColors: [
-                    .blue,
-                    .black,
-                    .black
-                ]
-            )
         }
     }
 }
